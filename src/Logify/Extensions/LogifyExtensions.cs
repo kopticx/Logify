74bb910fe3a170b2a20adba2e01d0a9ef4ec21e8
kopticx <fdzk100@gmail.com>
2025-01-04 22:38:32 -0600

    Add LogInformationCustom and LogErrorCustom methods
    Introduced new extension methods for structured logging: LogInformationCustom and LogErrorCustom. These methods allow logging custom objects along with tracker IDs and messages, with support for JSON serialization. This enhances the flexibility and clarity of log entries.

--- src/Logify/Extensions/LogifyExtensions.cs
+++ src/Logify/Extensions/LogifyExtensions.cs
@@ -38,6 +38,20 @@ public static class LogifyExtensions
   }
 
   /// <summary>
+  /// Logs information with a custom object and an accompanying message. The custom object is either serialized to JSON or used as a string based on its type and adds a trackerId.
+  /// </summary>
+  /// <param name="logger">The logger instance used to log the information.</param>
+  /// <param name="message">The message to be logged alongside the custom object.</param>
+  /// <param name="customObject">The custom object to be logged. If the object is of type string, it is logged as a message. Otherwise, it is serialized to JSON and logged.</param>
+  /// <typeparam name="T">The type of the custom object being logged.</typeparam>
+  public static void LogInformationCustom<T>(this ILogger logger, string message, T customObject)
+  {
+    var customMessageJson = GetMessageObject(message, customObject);
+
+    logger.Log(LogLevel.Information, customMessageJson);
+  }
+
+  /// <summary>
   /// Logs an error with a custom object and an accompanying exception. The custom object is either serialized to JSON or used as a string based on its type and adds a trackerId.
   /// </summary>
   /// <param name="logger">The logger instance used to log the error.</param>
@@ -60,6 +74,32 @@ public static class LogifyExtensions
     var customMessageJson = GetObjectLogging(customObject);
 
     logger.Log(LogLevel.Error, exception, customMessageJson);
+  }
+
+  /// <summary>
+  /// Logs an error with a custom object and an exception. The custom object is either serialized to JSON or used as a string based on its type and adds a trackerId.
+  /// </summary>
+  /// <param name="logger">The logger instance used to log the error.</param>
+  /// <param name="customObject">The custom object to be logged. If the object is of type string, it is logged as a message. Otherwise, it is serialized to JSON and logged.</param>
+  /// <param name="exception">The exception to be logged along with the custom object.</param>
+  /// <typeparam name="T">The type of the custom object being logged.</typeparam>
+  public static void LogErrorCustom<T>(this ILogger logger, string message, T customObject, Exception? exception)
+  {
+    var customMessageJson = GetMessageObject(message, customObject);
+
+    logger.Log(LogLevel.Error, exception, customMessageJson);
+  }
+
+  private static string GetMessageObject<T>(string message, T customObject)
+  {
+    var logObject = new Dictionary<string, object>
+    {
+      { "TrackerId", TrackedLogger.ExecutionName },
+      { "Message", message },
+      { "CustomObject", customObject }
+    };
+
+    return JsonSerializer.Serialize(logObject, _jsonSerializerOptions);
   }
 
   private static string GetStringObject(string message)
